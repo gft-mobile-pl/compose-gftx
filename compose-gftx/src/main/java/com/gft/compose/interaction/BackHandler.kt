7a510cbbbe62092be8803f2d2d7fa461e501d010
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2024-06-03 16:42:53 +0200

    Add doc to LifecycleAwareBackHandler

--- compose-gftx/src/main/java/com/gft/compose/interaction/BackHandler.kt
+++ compose-gftx/src/main/java/com/gft/compose/interaction/BackHandler.kt
@@ -6,6 +6,16 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 
+/**
+ * An effect for handling presses of the system back button.
+ *
+ * This method is similar to [BackHandler] however, it adds the possibility to choose the required minimum lifecycle state
+ * to propagate the back pressed event to the handler.
+ *
+ * Note that regardless of the value of `minActiveStateForEventsPropagation`, this composable intercepts the back pressed events
+ * whenever the current state is at least `Lifecycle.State.STARTED` (and `enabled` is set to `true`), but it simply does not pass the events to the handler
+ * if the current state is not at least `minActiveStateForEventsPropagation`.
+ */
 @Composable
 fun LifecycleAwareBackHandler(
     enabled: Boolean = true,
