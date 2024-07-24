79bf718c6309593f19d59748e1315653a4f742be
Szymon N <e-snni@gft.com>
2024-07-24 10:28:06 +0200

    [Maintenance] Add maven publishing configuration
    Added maven publishing configuration

--- compose-gftx/build.gradle.kts
+++ compose-gftx/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.vanniktech.maven.publish.AndroidMultiVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.maven.publish)
 }
 
 android {
@@ -32,6 +36,43 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.8"
+    }
+}
+
+mavenPublishing {
+    configure(
+        AndroidMultiVariantLibrary(
+            sourcesJar = true,
+            publishJavadocJar = true,
+        )
+    )
+
+    coordinates(project.property("libraryGroupId") as String, "compose", project.property("libraryVersion") as String)
+
+    pom {
+        name.set(project.property("libraryName") as String)
+        description.set(project.property("libraryDescription") as String)
+        inceptionYear.set(project.property("libraryInceptionYear") as String)
+        url.set("https://${project.property("libraryRepositoryUrl") as String}")
+        licenses {
+            license {
+                name.set(project.property("libraryLicenseName") as String)
+                url.set(project.property("libraryLicenseUrl") as String)
+                distribution.set(project.property("libraryLicenseDistribution") as String)
+            }
+        }
+        developers {
+            developer {
+                name.set(project.property("libraryDeveloperName") as String)
+            }
+        }
+        scm {
+            url.set("https://${project.property("libraryRepositoryUrl") as String}")
+            connection.set("scm:git:git://${project.property("libraryRepositoryUrl") as String}")
+            developerConnection.set("scm:git:ssh://git@${project.property("libraryRepositoryUrl") as String}.git")
+        }
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+        signAllPublications()
     }
 }
 
